64d7ff5dd1be01edcf60dfc58deee2f4636a1c45
JARS29 <jandresrsalas@gmail.com>
2020-04-03 19:18:42 -0500

    Updating  the set and get functions

--- UR3_start.py
+++ UR3_start.py
@@ -14,28 +14,35 @@ import time
 from operator import add
 import numpy as np
 
-def get_joint_handle(clientID, object_type=simConst.sim_object_joint_type, mode=sim.simx_opmode_oneshot_wait):
+opmode=sim.simx_opmode_oneshot_wait
+def get_joint_handle(clientID, object_type=simConst.sim_object_joint_type, mode=opmode):
     UR3_joints={} 
     _, ids,_,_,names= sim.simxGetObjectGroupData(clientID,object_type, 0,mode)
-    _, ids,_,rot,_= sim.simxGetObjectGroupData(clientID,object_type, 5,mode)
+    #_, ids,_,rot,_= sim.simxGetObjectGroupData(clientID,object_type, 5,mode)
     c=0
-    d=0
+    #d=0
     for i in names:
-        UR3_joints[i]=[ids[c], rot[d:d+3]]
+        UR3_joints[i]=[ids[c]]
+        #UR3_joints[i]=[ids[c], rot[d:d+3]]
         c=c+1    
-        d=d+3
+        #d=d+3
     return UR3_joints
 
-def set_joint_orientation(ClientID, JointID, angles, reference=-1, mode=sim.simx_opmode_oneshot_wait):
-    sim.simxSetJointTargetPosition(clientID, JointID, angles, sim.simx_opmode_oneshot_wait)
-    print('La junta ' + str(JointID) + ' se ha configurado en la siguiente orientacion ' + str(angles))
+def get_joint_orientation(ClientID,JointID, relativeTo=-1, mode= opmode):
+    return sim.simxGetObjectOrientation(ClientID, JointID, relativeTo, mode)[1]
+    
+
+def set_joint_orientation(ClientID, JointID, angle, mode=opmode):
+    sim.simxSetJointTargetPosition(clientID, JointID, np.deg2rad(angle), mode)
+    print('La junta ' + str(JointID) + ' se ha configurado en la siguiente orientacion ' + str(angle))
     return 0
 
-def set_joint_velocity(clientID, JointID, targetVelocity, mode=sim.simx_opmode_streaming):
+def set_joint_velocity(clientID, JointID, targetVelocity, mode=opmode):
     return 0
 
 
 try:
+    
     sim.simxFinish(-1) #Terminar todas las conexiones
     clientID=sim.simxStart('127.0.0.1',19997,True,True,5000,5) # Connect to CoppeliaSim 
     if clientID!=-1:
@@ -47,15 +54,27 @@ except:
 
 
 UR3_joints_IP=get_joint_handle(clientID) #Getting the reference
-print(UR3_joints_IP)
+for i in UR3_joints_IP:
+    UR3_joints_IP[i].append(get_joint_orientation(clientID, UR3_joints_IP[i][0]))
+    #print(get_joint_orientation(clientID, UR3_joints_IP[i]))
+
+         
 #J1z,J2z,J3Z
 #Practica 30 grados j1, 15 grados j2, 30 grados j3, 20 grados j4, 15 grados j5, 10 grados j6
-new_angles=[np.deg2rad(30),np.deg2rad(15),np.deg2rad(30),
-            np.deg2rad(20),np.deg2rad(15),np.deg2rad(10)]
+new_angles=[60,25,10,30,15,50]
 
 c=0
-for i in UR3_joints_IP:
-    set_joint_orientation(clientID, UR3_joints_IP[i][0],new_angles[c], ) #Todas las juntas posicion inicial
-    c=c+1
-    time.sleep(3)
 
+sim.simxSynchronous(clientID,True)
+sim.simxSynchronousTrigger(clientID) 
+sim.simxStartSimulation(clientID, sim.simx_opmode_blocking)
+
+
+for i in UR3_joints_IP:
+    set_joint_orientation(clientID, UR3_joints_IP[i][0],new_angles[c], mode=sim.simx_opmode_blocking) #Todas las juntas posicion inicial
+    c=c+1
+
+time.sleep(5)
+sim.simxStopSimulation(clientID, sim.simx_opmode_blocking)
+
+sim.simxFinish(clientID)
